6fdaeef530286e808286ce9f9054970d4324286f
Yoshiki-GODA <goudenriki9991@gmail.com>
2026-07-28 21:08:48 +0900

    Fix main_test.go

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestEvenOrOdd(t *testing.T) {
-	result := TestEvenOrOdd(11)
+	result := EvenOrOdd(11)
 	if result != "odd" {
 		t.Errorf("expected odd, acutal: %s", result)
 	}
